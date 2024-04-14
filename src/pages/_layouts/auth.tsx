d6af45e0d6ff71c79bf0cef2af680f5ec6116092
João Manoel <joaomanoeldev@gmail.com>
2024-04-14 12:47:20 -0300

    feat: implements layout to AuthLayout

--- src/pages/_layouts/auth.tsx
+++ src/pages/_layouts/auth.tsx
@@ -1,11 +1,22 @@
 import { Outlet } from "react-router-dom"
 
+import { Icons } from "@/components/icons"
+
 export const AuthLayout = () => {
   return (
-    <div>
-      <h1>Autenticação</h1>
+    <div className="min-h-screen grid grid-cols-2">
+      <div className="h-full border-r border-foreground/5 bg-muted p-10 text-muted-foreground flex flex-col justify-between">
+        <div className="flex items-center gap-3 text-lg text-foreground">
+          <Icons.pizza className="w-5 h-5" />
+          <span className="font-semibold">Pizza Shop</span>
+        </div>
 
-      <div>
+        <footer className="text-sm">
+          Painel do parceiro &copy; pizza shop - {new Date().getFullYear()}
+        </footer>
+      </div>
+
+      <div className="flex flex-col items-center justify-center">
        <Outlet />
       </div>
     </div>
