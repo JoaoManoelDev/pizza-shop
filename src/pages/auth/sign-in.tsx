28502261ec2836b317774c54fd4042e77bab1351
João Manoel <joaomanoeldev@gmail.com>
2024-04-14 12:48:17 -0300

    feat: add visual form to the sign-in

--- src/pages/auth/sign-in.tsx
+++ src/pages/auth/sign-in.tsx
@@ -1,10 +1,35 @@
 import { Helmet } from "react-helmet-async"
 
+import { Button } from "@/components/ui/button"
+import { Label } from "@/components/ui/label"
+import { Input } from "@/components/ui/input"
+
 export const SignIn = () => {
   return (
-    <>
+    < >
       <Helmet title="Login" />
-      <h1>Sing-in</h1>
+
+      <div className="p-8">
+        <div className="w-[350px] flex flex-col justify-center gap-6">
+          <div className="flex flex-col gap-2 text-center">
+            <h1 className="text-2xl font-semibold tracking-tighter">
+              Acessar Painel
+            </h1>
+            <p className="text-sm text-muted-foreground">
+              Acompanhe suas vendas pelo painel do parceiro
+            </p>
+          </div>
+          
+          <form className="space-y-4">
+            <div className="space-y-2">
+              <Label htmlFor="email">Seu e-mail</Label>
+              <Input id="email" type="email" />
+            </div>
+
+            <Button className="w-full" type="submit">Acessar Painel</Button>
+          </form>
+        </div>
+      </div>
     </>
   )
 }
