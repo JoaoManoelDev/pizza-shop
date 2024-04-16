378deca52e263cc67ad28c22ec4768850f00ee17
João Manoel <joaomanoeldev@gmail.com>
2024-04-16 11:43:28 -0300

    feat: add link to sign up page

--- src/pages/auth/sign-in.tsx
+++ src/pages/auth/sign-in.tsx
@@ -1,4 +1,5 @@
 import { Helmet } from "react-helmet-async"
+import { Link } from "react-router-dom"
 import { useForm } from "react-hook-form"
 import { z } from "zod"
 import { toast } from "sonner"
@@ -36,6 +37,16 @@ export const SignIn = () => {
       <Helmet title="Login" />
 
       <div className="p-8">
+        <Button
+          asChild
+          className="absolute right-8 top-8"
+          variant="link"
+        >
+          <Link to="/sign-up">
+            Novo estabelecimento
+          </Link>
+        </Button>
+
         <div className="w-[350px] flex flex-col justify-center gap-6">
           <div className="flex flex-col gap-2 text-center">
             <h1 className="text-2xl font-semibold tracking-tighter">
