3c48ca56ff64251ef487ded4a6945895dfbec06b
João Manoel <joaomanoeldev@gmail.com>
2024-04-16 19:42:53 -0300

    feat: modify styles

--- src/components/mode-toggle.tsx
+++ src/components/mode-toggle.tsx
@@ -16,9 +16,9 @@ export function ModeToggle() {
   return (
     <DropdownMenu>
       <DropdownMenuTrigger asChild>
-        <Button variant="outline" size="icon">
-          <Icons.sun className="h-[1.2rem] w-[1.2rem] rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0" />
-          <Icons.moon className="absolute h-[1.2rem] w-[1.2rem] rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100" />
+        <Button variant="outline" size="iconMd">
+          <Icons.sun className="w-5 h-5 rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0" />
+          <Icons.moon className="absolute w-5 h-5 rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100" />
           <span className="sr-only">Toggle theme</span>
         </Button>
       </DropdownMenuTrigger>
--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -13,13 +13,13 @@ import { ModeToggle } from "./mode-toggle"
 export const Navbar = () => {
   return (
     <nav className="fixed inset-y-0 left-0 z-10 hidden w-14 flex-col border-r bg-background sm:flex">
-      <TooltipProvider delayDuration={100}>
-        <div className="flex flex-col items-center gap-4 px-2 sm:py-5">
-          <Icons.pizza className="w-6 h-6" />
+      <div className="flex flex-col items-center gap-4 px-2 sm:py-5">
+        <Icons.pizza className="w-6 h-6" />
 
-          <Separator className="w-6" />
+        <Separator className="w-6" />
 
-          <div className="flex flex-col gap-4 px-2">
+        <div className="flex flex-col gap-4 px-2">
+          <TooltipProvider delayDuration={100}>
             <Tooltip>
               <TooltipTrigger>
                 <NavLink to="/">
@@ -39,19 +39,13 @@ export const Navbar = () => {
               </TooltipTrigger>
               <TooltipContent side="right">Vendas</TooltipContent>
             </Tooltip>
-
-          </div>
+          </TooltipProvider>
         </div>
+      </div>
 
-        <div className="flex flex-col items-center gap-4 px-2 sm:py-5 mt-auto">
-          <Tooltip>
-            <TooltipTrigger>
-              <ModeToggle />
-            </TooltipTrigger>
-            <TooltipContent side="right">Alternar Tema</TooltipContent>
-          </Tooltip>
-        </div>
-      </TooltipProvider>
+      <div className="flex flex-col items-center gap-4 px-2 sm:py-5 mt-auto">
+        <ModeToggle />
+      </div>
     </nav>
   )
 }
