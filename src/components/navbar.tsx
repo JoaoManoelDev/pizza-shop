edcaf4f75c2f735b312b550b20186b08fcfd4423
João Manoel <joaomanoeldev@gmail.com>
2024-04-16 15:56:42 -0300

    feat: modify Navbar and delete Header

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -1,18 +1,41 @@
-import { NavLink } from "@/components/nav-link"
 import { Icons } from "@/components/icons"
+
+import { Separator } from "@/components/ui/separator"
+import { Tooltip, TooltipContent, TooltipProvider, TooltipTrigger } from "./ui/tooltip"
+import { NavLink } from "./nav-link"
 
 export const Navbar = () => {
   return (
-    <nav className="flex items-center space-x-4 lg:space-x-6">
-      <NavLink to="/">
-        <Icons.home className="w-4 h-4" />
-        Início
-      </NavLink>
+    <nav className="fixed inset-y-0 left-0 z-10 hidden w-14 flex-col border-r bg-background sm:flex">
+      <div className="flex flex-col items-center gap-4 px-2 sm:py-5">
+        <Icons.pizza className="w-6 h-6" />
 
-      <NavLink to="/orders">
-        <Icons.utensilsCrossed className="w-4 h-4" />
-        Pedidos
-      </NavLink>
+        <Separator className="w-6" />
+
+        <div className="flex flex-col gap-4 px-2">
+          <TooltipProvider delayDuration={100}>
+            <Tooltip>
+              <TooltipTrigger>
+                <NavLink to="/">
+                  <Icons.home className="w-5 h-5 transition-all group-hover:scale-110" />
+                  <span className="sr-only">dashboard</span>
+                </NavLink>
+              </TooltipTrigger>
+              <TooltipContent side="right">Início</TooltipContent>
+            </Tooltip>
+
+            <Tooltip>
+              <TooltipTrigger>
+                <NavLink to="/orders">
+                  <Icons.utensilsCrossed className="w-5 h-5 transition-all group-hover:scale-110" />
+                  <span className="sr-only">pizzashop</span>
+                </NavLink>
+              </TooltipTrigger>
+              <TooltipContent side="right">Vendas</TooltipContent>
+            </Tooltip>
+          </TooltipProvider>
+        </div>
+      </div>
     </nav>
   )
 }
--- src/pages/_layouts/app.tsx
+++ src/pages/_layouts/app.tsx
@@ -1,13 +1,13 @@
 import { Outlet } from "react-router-dom"
 
-import { Header } from "@/components/header"
+import { Navbar } from "@/components/navbar"
 
 export const AppLayout = () => {
   return (
     <div className="flex flex-col min-h-screen antialiased">
-      <Header />
+      <Navbar />
 
-      <div className="flex flex-1 flex-col gap-4 p-8 pt-6">
+      <div className="flex flex-1 flex-col gap-4 p-8 pt-6 ml-14">
        <Outlet />
       </div>
     </div>
