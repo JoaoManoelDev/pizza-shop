8a626db464aed124b659f5d2d5afe131cecd93c5
João Manoel <joaomanoeldev@gmail.com>
2024-04-17 02:00:27 -0300

    refactor: remove Header

--- src/components/header.tsx
+++
@@ -1,9 +0,0 @@
-import { AccountMenu } from "./account-menu"
-
-export const Header = () => {
-  return (
-    <header className="flex justify-end">
-      <AccountMenu />
-    </header>
-  )
-}
--- src/pages/_layouts/app.tsx
+++ src/pages/_layouts/app.tsx
@@ -1,7 +1,6 @@
 import { Outlet } from "react-router-dom"
 
 import { Navbar } from "@/components/navbar"
-import { Header } from "@/components/header"
 
 export const AppLayout = () => {
   return (
@@ -9,7 +8,6 @@ export const AppLayout = () => {
       <Navbar />
 
       <div className="flex flex-1 flex-col gap-4 p-8 pt-6 ml-14">
-        <Header />
         <Outlet />
       </div>
     </div>
