3afc689d1478b28fdd64af4200d24503a4f30a71
João Manoel <joaomanoeldev@gmail.com>
2024-04-17 01:32:09 -0300

    feat: add Header to AppLayout

--- src/pages/_layouts/app.tsx
+++ src/pages/_layouts/app.tsx
@@ -1,14 +1,16 @@
 import { Outlet } from "react-router-dom"
 
 import { Navbar } from "@/components/navbar"
+import { Header } from "@/components/header"
 
 export const AppLayout = () => {
   return (
-    <div className="flex flex-col min-h-screen antialiased">
+    <div className="flex flex-col min-h-screen antialiased relative">
       <Navbar />
 
       <div className="flex flex-1 flex-col gap-4 p-8 pt-6 ml-14">
-       <Outlet />
+        <Header />
+        <Outlet />
       </div>
     </div>
   )
