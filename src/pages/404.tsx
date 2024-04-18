687964f28f4d495de22bdea214fe6b724dda27b7
João Manoel <joaomanoeldev@gmail.com>
2024-04-18 17:53:24 -0300

    fix: add correct redirect path

--- src/pages/404.tsx
+++ src/pages/404.tsx
@@ -5,7 +5,7 @@ export const NotFound = () => {
     <div className="flex h-screen flex-col items-center justify-center gap-2">
       <h1 className="text-4xl font-bold">Página não encontrada</h1>
       <p className="text-accent-foreground">
-        Voltar para o <Link to="dashboard" className="text-sky-500">Dashboard</Link>
+        Voltar para o <Link to="/" className="text-sky-500">Dashboard</Link>
       </p>
     </div>
   )
