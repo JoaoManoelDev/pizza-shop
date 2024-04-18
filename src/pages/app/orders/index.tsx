adcf0dcc15b674e19f8dc1ed7c915453b7945c91
João Manoel <joaomanoeldev@gmail.com>
2024-04-18 13:55:54 -0300

    feat: add Pagination to orders page

--- src/pages/app/orders/index.tsx
+++ src/pages/app/orders/index.tsx
@@ -9,6 +9,7 @@ import {
   TableHeader,
   TableRow
 } from "@/components/ui/table"
+import { Pagination } from "@/components/pagination"
 
 export const Orders = () => {
   return (
@@ -17,34 +18,35 @@ export const Orders = () => {
 
       <div className="flex flex-col gap-4">
         <h1 className="text-3xl font-bold tracking-tighter">Pedidos</h1>
-      </div>
 
-      <div className="space-y-2.5">
-        <OrderTableFilters />
+        <div className="space-y-2.5">
+          <OrderTableFilters />
 
-        <div className="border rounded-md">
-          <Table>
-            <TableHeader>
-              <TableRow>
-                <TableHead className="w-[64px]"></TableHead>
-                <TableHead className="w-[140px]">ID</TableHead>
-                <TableHead className="w-[180px]">Realizado há</TableHead>
-                <TableHead className="w-[140px]">Status</TableHead>
-                <TableHead>Client</TableHead>
-                <TableHead className="w-[140px]">Total do pedido</TableHead>
-                <TableHead className="w-[164px]"></TableHead>
-                <TableHead className="w-[132px]"></TableHead>
-              </TableRow>
-            </TableHeader>
+          <div className="border rounded-md">
+            <Table>
+              <TableHeader>
+                <TableRow>
+                  <TableHead className="w-[64px]"></TableHead>
+                  <TableHead className="w-[140px]">ID</TableHead>
+                  <TableHead className="w-[180px]">Realizado há</TableHead>
+                  <TableHead className="w-[140px]">Status</TableHead>
+                  <TableHead>Client</TableHead>
+                  <TableHead className="w-[140px]">Total do pedido</TableHead>
+                  <TableHead className="w-[164px]"></TableHead>
+                  <TableHead className="w-[132px]"></TableHead>
+                </TableRow>
+              </TableHeader>
 
-            <TableBody>
-              {Array.from({ length: 10 }).map((_, i) => {
-                return (
-                  <OrderTableRow key={i} />
-                )
-              })}
-            </TableBody>
-          </Table>
+              <TableBody>
+                {Array.from({ length: 10 }).map((_, i) => {
+                  return (
+                    <OrderTableRow key={i} />
+                  )
+                })}
+              </TableBody>
+            </Table>
+          </div>
+          <Pagination pageIndex={0} totalCount={105} perPage={10} />
         </div>
       </div>
     </>
