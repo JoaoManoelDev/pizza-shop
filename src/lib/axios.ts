8ee8fca0958b257938f1bf96652194ecce74c674
João Manoel <joaomanoeldev@gmail.com>
2024-04-22 18:50:44 -0300

    chore: add delay to all requests

--- src/lib/axios.ts
+++ src/lib/axios.ts
@@ -3,5 +3,13 @@ import axios from "axios"
 
 export const api = axios.create({
   baseURL: env.VITE_API_URL,
-  withCredentials: true
+  withCredentials: true,
 })
+
+if (env.VITE_ENABLE_API_DELAY) {
+  api.interceptors.request.use(async (config) => {
+    await new Promise(resolve => setTimeout(resolve, 4000))
+
+    return config
+  })
+}
