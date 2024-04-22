6ff6a60ab562507df8cd5307959202ed2384f961
João Manoel <joaomanoeldev@gmail.com>
2024-04-22 17:46:02 -0300

    feat: search profile and managed restaurant

--- src/components/account-menu.tsx
+++ src/components/account-menu.tsx
@@ -1,3 +1,5 @@
+import { useQuery } from "@tanstack/react-query"
+
 import {
   DropdownMenu,
   DropdownMenuContent,
@@ -8,20 +10,39 @@ import {
 } from "@/components/ui/dropdown-menu"
 import { Button } from "@/components/ui/button"
 import { Icons } from "@/components/icons"
+import { getProfile } from "@/api/get-profile"
+import { getManagedRestaurant } from "@/api/get-managed-restaurant"
+
 
 export const AccountMenu = () => {
+  const { data: profile } = useQuery({
+    queryKey: ["profile"],
+    queryFn: getProfile,
+  })
+
+  const { data: managedRestaurant } = useQuery({
+    queryKey: ["managed-restaurant"],
+    queryFn: getManagedRestaurant,
+  })
+
   return (
     <DropdownMenu>
       <DropdownMenuTrigger asChild>
         <Button variant="outline" size="iconMd" className="flex items-center gap-2 select-none">
-          <Icons.settings className="w-5 h-5" />
+          <Icons.store className="w-5 h-5" />
         </Button>
       </DropdownMenuTrigger>
 
       <DropdownMenuContent side="right" align="end" className="w-56">
+        <p className="text-xl text-center font-bold">{managedRestaurant?.name}</p>
+          
+        <DropdownMenuSeparator />
+        
         <DropdownMenuLabel className="flex flex-col">
-          <span>João Manoel</span>
-          <span className="text-xs font-normal text-muted-foreground">joao@gmail.com</span>
+          <span>{profile?.name}</span>
+          <span className="text-xs font-normal text-muted-foreground">
+            {profile?.email}
+          </span>
         </DropdownMenuLabel>
 
         <DropdownMenuSeparator />
