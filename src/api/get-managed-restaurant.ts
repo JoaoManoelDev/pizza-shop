29948c8d8142265583be0b6a9d228f0128aa9be5
João Manoel <joaomanoeldev@gmail.com>
2024-04-22 17:44:49 -0300

    feat: create getManagedRestaurant function

---
+++ src/api/get-managed-restaurant.ts
@@ -0,0 +1,18 @@
+import { api } from "@/lib/axios"
+
+interface GetManagedRestaurantResponse {
+  name: string
+  id: string
+  createdAt: Date | null
+  updatedAt: Date | null
+  description: string | null
+  managerId: string | null
+}
+
+export const getManagedRestaurant = async () => {
+  const response = await api.get<GetManagedRestaurantResponse>(
+    "/managed-restaurant"
+  )
+  
+  return response.data
+}
