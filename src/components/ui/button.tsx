2d4b5ec029c3ba6a012e10b33cd50cdf0c0c3636
João Manoel <joaomanoeldev@gmail.com>
2024-04-23 00:02:10 -0300

    feat: add success variant

--- src/components/ui/button.tsx
+++ src/components/ui/button.tsx
@@ -10,6 +10,8 @@ const buttonVariants = cva(
     variants: {
       variant: {
         default: "bg-primary text-primary-foreground hover:bg-primary/90",
+        success:
+        "bg-emerald-500 text-white hover:bg-emerald-500/90 dark:bg-emerald-600 dark:hover:bg-emerald-600/80",
         destructive:
           "bg-destructive text-destructive-foreground hover:bg-destructive/90",
         outline:
