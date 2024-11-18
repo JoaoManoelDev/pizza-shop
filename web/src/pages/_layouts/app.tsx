d19aa143e87f3d7040b95bab6ad3aed4d1e5ea8c
João Manoel <joaomanoeldev@gmail.com>
2024-11-18 19:07:42 -0300

    feat: automatic log-out

--- web/src/pages/_layouts/app.tsx
+++ web/src/pages/_layouts/app.tsx
@@ -1,8 +1,33 @@
-import { Outlet } from "react-router-dom"
+import { useEffect } from "react"
+import { Outlet, useNavigate } from "react-router-dom"
 
 import { Navbar } from "@/components/navbar"
+import { api } from "@/lib/axios"
+import { isAxiosError } from "axios"
 
 export const AppLayout = () => {
+  const navigate = useNavigate()
+
+  useEffect(() => {
+    const interceptorId = api.interceptors.response.use(
+      response => response,
+      error => {
+        if (isAxiosError(error)) {
+          const status = error.response?.status
+          const code = error.response?.data.code
+
+          if (status === 401 && code === 'UNAUTHORIZED') {
+            navigate('sign-in', { replace: true })
+          }
+        }
+      }
+    )
+
+    return () => {
+      api.interceptors.response.eject(interceptorId)
+    }
+  }, [navigate])
+
   return (
     <div className="flex flex-col min-h-screen antialiased relative">
       <Navbar />
